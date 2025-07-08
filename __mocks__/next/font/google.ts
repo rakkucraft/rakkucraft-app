89efd9a936c84e1b826251be9c82322a35986f8a
YuTaExtend <yuta.extend@gmail.com>
2025-07-08 10:21:00 +0900

    test font.ts

---
+++ __mocks__/next/font/google.ts
@@ -0,0 +1,11 @@
+export const Rubik = jest.fn(() => ({
+  className: "mocked-rubik",
+}));
+
+export const Noto_Sans_JP = jest.fn(() => ({
+  className: "mocked-noto-sans-jp",
+}));
+
+export const Inter = jest.fn(() => ({
+  className: "mocked-inter",
+}));
